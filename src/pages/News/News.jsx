b83ffe7f719a3cfbffc0d443c435828c0900a64d
LancerHawk <141824147+Lancerhawk@users.noreply.github.com>
2025-03-09 14:04:13 +0530

    Update News.jsx

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -13,6 +13,7 @@ function News() {
     const response = await fetch(
       `https://newsapi.org/v2/everything?q=${query}&language=en&sortBy=publishedAt&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`
     );
+    console.log("API Key:", process.env.NEXT_PUBLIC_API_KEY);
     const data = await response.json();
     if (data.articles) {
       setNews(
